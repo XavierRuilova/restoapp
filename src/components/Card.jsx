a36e1aef7ff18afdd72f19b8cc3b234cfd6310d7
Xavier Ruilova <xaruilova.dwfsc12@bootcamp.ucamp.io>
2023-02-07 17:14:10 -0500

    primer commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card({image, title, description}) {
   return (
     <div className='card'>
-        <img src={image} />
+        <img src={image} alt='image'/>
         <h3>{title}</h3>
         <p>{description}</p>
     </div>
