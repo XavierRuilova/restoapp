bedca517efa122e05a87d9f0b3badfa62f1ad860
Xavier Ruilova <xaruilova.dwfsc12@bootcamp.ucamp.io>
2023-02-07 17:28:39 -0500

    deploy

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card({pic, title, description}) {
   return (
     <div className='card'>
-        <img src={pic} alt='image'/>
+        <img src={pic} alt='ilustracion'/>
         <h3>{title}</h3>
         <p>{description}</p>
     </div>
